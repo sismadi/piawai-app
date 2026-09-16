e8b8ad4ef7f62b55c52f742d01fe981c27703c13
Wawan Sismadi <wawan@sismadi.com>
2026-09-16 14:42:55 +0700

    Update db.js

--- db.js
+++ db.js
@@ -18,7 +18,7 @@
 
 // GANTI dengan URL hasil `wrangler deploy` di repo cms-api, tanpa slash
 // di akhir. Contoh: 'https://cms-api.namaakun.workers.dev'
-const API_BASE = 'https://cms-api.piawai.workers.dev';
+const API_BASE = 'https://piawai-api.piawai.workers.dev';
 
 const SCOPED_TABLES = new Set(['post']); // `users` & `komentar` tidak lagi diakses lewat /api
 
